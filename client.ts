977a61478f58cdc6416a6c64b8becfcda2995a79
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2023-04-10 17:20:29 +0700

    accept book input from process arguments

--- client.ts
+++ client.ts
@@ -10,18 +10,30 @@ const bookPackage = bookProto.bookPackage
 
 const client = new bookPackage.BookService("0.0.0.0:40000", grpc.credentials.createInsecure())
 
-client.createBook({
-  "id": -1,
-  "title": "Harry Potter by J. K. Rowling",
-  "page": 40,
-}, (err: Error | null, response: any) => {
-  if (err) {
-    console.log(`Received error: ${err.message}`)
-  } else {
-    // console.log(`Received from server: ${JSON.stringify(response)}`);
-    console.log(response)
-  }
-})
+const bookTitle: string = process.argv[2]
+const totalPage: number = process.argv[3] as unknown as number
+
+if (bookTitle == null) {
+  console.error(`Parameter title is required!`)
+}
+if (totalPage === undefined) {
+  console.error(`Parameter total of page is required!`)
+}
+
+if (bookTitle && totalPage) {
+  client.createBook({
+    "id": -1,
+    "title": bookTitle,
+    "page": totalPage
+  }, (err: Error | null, response: any) => {
+    if (err) {
+      console.log(`Received error: ${err.message}`)
+    } else {
+      // console.log(`Received from server: ${JSON.stringify(response)}`);
+      console.log(response)
+    }
+  })
+}
 
 client.getBooks({}, (err: Error | null, response: any) => {
   if (err) {
